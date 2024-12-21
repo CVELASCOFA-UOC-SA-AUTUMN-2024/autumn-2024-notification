27eea690127f5d9f5b64e692d1ce3352bfccf1a9
cvelascofa <cvelascofa@uoc.edu>
2024-12-21 08:55:25 +0100

    initial commit

---
+++ src/main/java/edu/uoc/epcsd/notification/domain/service/NotificationServiceImpl.java
@@ -0,0 +1,32 @@
+package edu.uoc.epcsd.notification.domain.service;
+
+import edu.uoc.epcsd.notification.application.kafka.ProductMessage;
+import edu.uoc.epcsd.notification.application.rest.dtos.GetProductResponse;
+import edu.uoc.epcsd.notification.application.rest.dtos.GetUserResponse;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDate;
+
+@Log4j2
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    @Value("${userService.getUsersToAlert.url}")
+    private String userServiceUrl;
+
+    @Value("${productService.getProductDetails.url}")
+    private String productServiceUrl;
+
+    @Override
+    public void notifyProductAvailable(ProductMessage productMessage) {
+            GetProductResponse productResponse = new RestTemplate().getForEntity(productServiceUrl, GetProductResponse.class, productMessage.getProductId()).getBody();
+            GetUserResponse[] usersToAlert = new RestTemplate().getForEntity(userServiceUrl, GetUserResponse[].class, productMessage.getProductId(), LocalDate.now()).getBody();
+
+        for (GetUserResponse user : usersToAlert) {
+            log.info("Sending email to user " + user.getFullName() + " email " + user.getEmail() + " to notify that we have availability of the product " + productResponse.getName());
+        }
+    }
+}
